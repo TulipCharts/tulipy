1c171a7a6f36a0d0f82a3f572764245aa26df829
alexpl52 <apolishchuk52@gmail.com>
2019-04-04 18:44:08 +0300

    Add test for pad_none

--- tests/test.py
+++ tests/test.py
@@ -11,23 +11,22 @@ CLOSE = np.array([81.59, 81.06, 82.87, 83,    83.61,
 
 class TestIndicators(unittest.TestCase):
 
+    EXPECTED = np.array([82.426, 82.738, 83.094, 83.318, 83.628,
+                         83.778, 84.254, 84.994, 85.574, 86.218,
+                         86.804]),
+
     def test_sma(self):
-        expected = np.array([82.426, 82.738, 83.094, 83.318, 83.628,
-                             83.778, 84.254, 84.994, 85.574, 86.218,
-                             86.804]),
+        actual = ti.sma(CLOSE, period=5, pad_none=False)
+        self.assertTrue(np.allclose(actual, self.EXPECTED))
 
-        actual = ti.sma(CLOSE, period=5)
+    def test_sma_accept_series(self):
+        actual = ti.sma(pd.Series(CLOSE), period=5, pad_none=False)
+        self.assertTrue(np.allclose(actual, self.EXPECTED))
 
-        self.assertTrue(np.allclose(actual, expected))
-
-    def test_sma_with_pandas(self):
-        expected = np.array([82.426, 82.738, 83.094, 83.318, 83.628,
-                             83.778, 84.254, 84.994, 85.574, 86.218,
-                             86.804]),
-
+    def test_sma_output_len_equal_input(self):
         actual = ti.sma(pd.Series(CLOSE), period=5)
-
-        self.assertTrue(np.allclose(actual, expected))
+        print(actual)
+        self.assertEqual(len(actual), len(CLOSE))
 
 
 if __name__ == '__main__':
