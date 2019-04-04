7649ec185f2103f25d632accfc9e2be60ecc4911
alexpl52 <apolishchuk52@gmail.com>
2019-04-04 15:12:48 +0300

    Add test for pandas integration

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,13 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 import tulipy as ti
 
 CLOSE = np.array([81.59, 81.06, 82.87, 83,    83.61,
                   83.15, 82.84, 83.99, 84.55, 84.36,
                   85.53, 86.54, 86.89, 87.77, 87.29])
+
 
 class TestIndicators(unittest.TestCase):
 
@@ -18,6 +20,15 @@ class TestIndicators(unittest.TestCase):
 
         self.assertTrue(np.allclose(actual, expected))
 
+    def test_sma_with_pandas(self):
+        expected = np.array([82.426, 82.738, 83.094, 83.318, 83.628,
+                             83.778, 84.254, 84.994, 85.574, 86.218,
+                             86.804]),
+
+        actual = ti.sma(pd.Series(CLOSE), period=5)
+
+        self.assertTrue(np.allclose(actual, expected))
+
+
 if __name__ == '__main__':
     unittest.main()
-
